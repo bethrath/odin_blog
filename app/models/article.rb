893caed25667e1a304c9d9bf3cc1ed06973b6248
Beth <bethanyrathbone@hotmail.com>
2017-02-24 14:25:08 +0000

    added a few gems (images)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
-class Article < ApplicationRecord
-	has_many :comments
+class Article < ActiveRecord::Base
+  has_many :comments
+  
+has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+
 end
